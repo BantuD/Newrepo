b204dddd1b6e548f4f743b3be8847bca946dc371
BantuD <500069424@stu.upes.ac.in>
2019-02-20 20:13:28 +0530

    First commit

---
+++ file1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class bantu
+{ 
+	private:
+		int x;
+	public:
+		bantu(int k)
+		{ x = k;
+		}
+};
+int main()
+{
+	bantu p1;
+	cout<<p1.bantu(25);
+return 0;
+}
+	
+
